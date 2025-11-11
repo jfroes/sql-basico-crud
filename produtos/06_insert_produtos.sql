02a218947552d42ccbbde3aa6135e4e4b4098ec7
José Froes <josepaulo.froes@gmail.com>
2025-11-11 10:02:51 -0300

    Ex_6: insert 5 products

---
+++ produtos/06_insert_produtos.sql
@@ -0,0 +1,16 @@
+-- INSIRA 5 PRODUTOS
+
+INSERT INTO products (nome, preco, estoque)
+VALUES ('Camisa Azul Goiaba', 199.99, 50);
+
+INSERT INTO products (nome, preco, estoque)
+VALUES ('Sapato Verde Morano', 799.99, 23);
+
+INSERT INTO products (nome, preco, estoque)
+VALUES ('Chapéu Rosa Limonada', 89.99, 14);
+
+INSERT INTO products (nome, preco, estoque)
+VALUES ('Cinto Roxo Melancia', 69.99, 41);
+
+INSERT INTO products (nome, preco, estoque)
+VALUES ('Calça Marrom Beterraba', 379.99, 37);
